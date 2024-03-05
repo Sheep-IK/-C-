1427fd4530caab0ee6a8f0ad1e2d8ab7a0d4c507
Sheep-IK <144210557+Sheep-IK@users.noreply.github.com>
2024-03-05 16:13:30 +0900

    210305 c++ 공부
    lecture 2까지 나감 ( namespace )

--- 240305.cpp
+++ 240305.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "animal.h"
 
+namespace animal {
+	int func() {
+		rabbit();
+		return 0;
+	}
+}
+//using animal::func;
 
 int main() {
+	rabbit();
 	std::cout << "반갑다 세상아 나는 휴학생이다!" << std::endl;
 	return 0;
 }
---
+++ animal.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+
+void rabbit() {
+	std::cout << "I AM Rabbit" << std::endl;
+}
