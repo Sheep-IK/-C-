eb06a095e96512bfb4017fac74a9b1fc6ebf48f5
Sheep-IK <144210557+Sheep-IK@users.noreply.github.com>
2024-03-08 17:05:13 +0900

    Leference 완료
    <2. C++ 참조자(레퍼런스)의 도입> 끝

---
+++ 240308-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int change_val(int& a) {
+	a = 10;
+
+	return 0;
+}
+
+int main() {
+	int soon_change = 5;
+
+	std::cout<< soon_change << std::endl;
+	change_val(soon_change);
+	std::cout << soon_change << std::endl;
+
+	return 0;
+}
